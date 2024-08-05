90471dd520218bc8fecf8e42879cf4763c0e93cc
Sharath Sunil <alphatrinity17@gmail.com>
2024-08-05 21:23:21 +0530

    New date feature added

--- age.py
+++ age.py
@@ -1,4 +1,6 @@
-print("Welcome to age verify")
+from datetime import date
+
+print(f"Welcome to age verify [{date.today()}]")
 age = int(input("Enter the age:"))
 if age>17:
 	print("You're a adult")
